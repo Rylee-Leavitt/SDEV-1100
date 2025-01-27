ae5e9538c9f5f8f0f15351398f2de2822678cbfe
Rylee Leavitt <ryleeleavitt@westernwyoming.edu>
2025-01-27 03:19:23 +0000

    psuedocode

--- SDEV1100 1st project/main.py
+++ SDEV1100 1st project/main.py
@@ -1,10 +1,49 @@
 #
-# Name
-# Date
-# Pet Class Programming Project
-# SDEV 1200
+# Rylee Leavitt
+# 1/26/2025
+# SDEV1100 1st project
+# SDEV 1100
 #
 
-# Use comments liberally throughout the program. 
+# Module 3 Assignment: Pseudo Code 1/26/2025
+    # write as much pseudo code as you can for this project. 
 
-import pet
+# Import necessary libraries
+
+# FOR each directory below the current one,
+
+    # FOR each file in the directory,
+
+       # IF the file is an Excel file,
+
+            # DO begin functions/call function
+
+# Define function to read and standardize FOR each excel file,
+
+    # Read/get data from the Excel file
+
+    # Standardize the column names
+    
+    # Return the standardized DataFrame
+
+# Define function to consolidate data from multiple Excel files
+    
+    # Iterate over all files in the directory
+
+            # Read and standardize the data
+
+            # Append to the consolidated DataFrame
+
+            # # Return the consolidated DataFrame
+
+# Define function to save consolidated data to a CSV file
+
+    # Open new CSV file and write data
+
+    # Define the output CSV file path
+    
+    # Consolidate the data
+    
+    # Save the consolidated data to a CSV file
+   
+# Execute the main function
